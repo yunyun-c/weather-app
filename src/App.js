dff0be013697723449e0471d6e084aa133d8f3da
cassie_c <cassie.nightingale.13.13@gmail.com>
2023-12-11 16:57:29 -0600

    deleted old files

--- src/App.js
+++ src/App.js
@@ -1,92 +1,174 @@
-import "./App.css";
-import cloudBackground from "./img/Cloud-background.png";
-import shower from "./img/Shower.png";
+import { useEffect, useState } from "react";
+import { images } from "./Images";
 
-function App() {
+let api_key = "d9e41930c6cec91658673cbda0952d92";
+
+export default function App() {
+  const [city, setCity] = useState("Taipei");
+  const [date, setDate] = useState("");
+  const [temp, setTemp] = useState(null);
+  const [unit, setUnit] = useState("C");
+  const [weather, setWeather] = useState("");
+
+  const celsiusToFahrenheit = (temp) => {
+    return (temp * 9) / 5 + 32;
+  };
+
+  useEffect(
+    function () {
+      async function weatherAPI() {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+        );
+        const data = await res.json();
+
+        console.log(data.weather);
+
+        if (data.dt) {
+          const unixTimestamp = data.dt;
+          const date = new Date(unixTimestamp * 1000);
+          const options = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          };
+          const formattedDate = date.toLocaleDateString("en-US", options);
+          setDate(formattedDate);
+        }
+
+        if (data.main && data.main.temp) {
+          setTemp((data.main.temp - 273.25).toFixed(1));
+        }
+
+        if (data.weather) {
+          const weatherDetail = data.weather[0].main;
+          setWeather(weatherDetail);
+        }
+      }
+      weatherAPI();
+    },
+    [city, date, temp]
+  );
+
   return (
     <div className="App">
+      <Sidebar
+        city={city}
+        date={date}
+        temp={temp}
+        unit={unit}
+        weather={weather}
+      />
+      <Main />
+    </div>
+  );
+}
+
+function Sidebar({ city, date, temp, unit, weather }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <section className="left">
       <div className="local">
-        <div className="search-nav hidden">
-          <button className="btn-close">X</button>
-          <div className="navSearch">
-            <input type="search" id="search" placeholder="search location" />
-            <span class="material-symbols-outlined">search</span>
-            <button className="submitBtn" type="submit">
-              Search
-            </button>
-            <div className="options">
-              <ul>
-                <li>city</li>
-                <li>city</li>
-                <li>city</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="header-btn">
-          <button id="btn-search-places">Seach for places</button>
-          <button id="btn-target">
-            <span class="material-icons-outlined">my_location</span>
-          </button>
-        </div>
+        {isOpen ? (
+          <SearchBtn setIsOpen={setIsOpen} />
+        ) : (
+          <Search setIsOpen={setIsOpen} />
+        )}
+
         <div className="local-weather-img">
           <img
             className="background-image"
-            src={cloudBackground}
+            src={images.cloudBackground}
             alt="background"
           ></img>
-
-          <img className="weather-image" src={shower} alt="weather-image"></img>
+          <div className="weather-image">
+            <WeatherImage />
+          </div>
         </div>
 
         <div className="details">
           <h1 className="temperature">
-            15<span>°C</span>
+            {temp}
+            <span>°{unit}</span>
           </h1>
-          <h2 className="type">Shower</h2>
-          <p className="date">Today . Fri, 5 Jun</p>
+          <h2 className="type">{weather}</h2>
+          <p className="date">{date}</p>
           <p className="location">
-            <span class="material-icons-outlined">room</span> my location
+            <span className="material-icons-outlined">room</span>
+            {city}
           </p>
         </div>
       </div>
-      <div className="next-5-days">
-        <div className="day">
-          <p>Tomorrow</p>
-          <img src={shower} alt="shower"></img>
-          <p>
-            16°C <span>11°C</span>
-          </p>
-        </div>
-        <div className="day">
-          <p>Tomorrow</p>
-          <img src={shower} alt="shower"></img>
-          <p>
-            16°C <span>11°C</span>
-          </p>
-        </div>
-        <div className="day">
-          <p>Tomorrow</p>
-          <img src={shower} alt="shower"></img>
-          <p>
-            16°C <span>11°C</span>
-          </p>
-        </div>
-        <div className="day">
-          <p>Tomorrow</p>
-          <img src={shower} alt="shower"></img>
-          <p>
-            16°C <span>11°C</span>
-          </p>
-        </div>
-        <div className="day">
-          <p>Tomorrow</p>
-          <img src={shower} alt="shower"></img>
-          <p>
-            16°C <span>11°C</span>
-          </p>
-        </div>
+    </section>
+  );
+}
+
+function WeatherImage() {
+  return <img src={images.shower} alt="weather-image" />;
+}
+
+function SearchBtn({ setIsOpen }) {
+  return (
+    <div className="header-btn">
+      <button id="btn-search-places" onClick={() => setIsOpen((open) => !open)}>
+        Seach for places
+      </button>
+      <button id="btn-target">
+        <span className="material-icons-outlined">my_location</span>
+      </button>
+    </div>
+  );
+}
+
+function Search({ setIsOpen }) {
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <>
+      <div className="search-nav">
+        <button
+          className="btn-close"
+          onClick={() => setIsOpen((open) => !open)}
+        >
+          X
+        </button>
+        <from className="navSearch">
+          <div>
+            <input
+              type="search"
+              id="search"
+              placeholder="search location"
+              vlaue={searchInput}
+              onChange={(e) => {
+                setSearchInput(e.target.value);
+              }}
+            />
+            <span className="material-symbols-outlined">search</span>
+            <button className="submitBtn" type="submit">
+              Search
+            </button>
+          </div>
+
+          <div className="options">
+            <ul>
+              <li>city</li>
+              <li>city</li>
+              <li>city</li>
+            </ul>
+          </div>
+        </from>
       </div>
+    </>
+  );
+}
+
+function Main() {
+  return (
+    <section className="right">
+      <UnitChange />
+      <FiveDaysReport />
       <div className="hightlight">
         <h3>Today's Hightlights</h3>
         <div className="hightlights">
@@ -130,8 +212,57 @@ function App() {
       <footer>
         <p>created by username - devChallenges.io</p>
       </footer>
+    </section>
+  );
+}
+
+function UnitChange() {
+  return (
+    <div className="temperatureUnit show">
+      <button id="c-btn">°C</button>
+      <button id="f-btn">°F</button>
     </div>
   );
 }
 
-export default App;
+function FiveDaysReport() {
+  return (
+    <div className="next-5-days">
+      <div className="day">
+        <p>Tomorrow</p>
+        <WeatherImage />
+        <p>
+          16°C <span>11°C</span>
+        </p>
+      </div>
+      <div className="day">
+        <p>Tomorrow</p>
+        <WeatherImage />
+        <p>
+          16°C <span>11°C</span>
+        </p>
+      </div>
+      <div className="day">
+        <p>Tomorrow</p>
+        <WeatherImage />
+        <p>
+          16°C <span>11°C</span>
+        </p>
+      </div>
+      <div className="day">
+        <p>Tomorrow</p>
+        <WeatherImage />
+        <p>
+          16°C <span>11°C</span>
+        </p>
+      </div>
+      <div className="day">
+        <p>Tomorrow</p>
+        <WeatherImage />
+        <p>
+          16°C <span>11°C</span>
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/Images.js
@@ -0,0 +1,17 @@
+// d9e41930c6cec91658673cbda0952d92
+
+export const images = {
+    cloudBackground: require("./img/Cloud-background.png"),
+    shower: require("./img/Shower.png"),
+    clear: require("./img/Clear.png"),
+    hail: require("./img/Hail.png"),
+    heavyCloud: require("./img/HeavyCloud.png"),
+    heavyRain: require("./img/HeavyRain.png"),
+    lightCloud: require("./img/LightCloud.png"),
+    lightRain: require("./img/LightRain.png"),
+    sleet: require("./img/Sleet.png"),
+    snow: require("./img/Snow.png"),
+    thunderstorm: require("./img/Thunderstorm.png")
+  };
+
+  
